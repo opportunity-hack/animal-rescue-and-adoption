0c15bf044fb3ec3ddc2c67c0863a6359b4400f2c
materwelon <materwelondhruv@gmail.com>
2025-02-01 18:46:11 -0700

    WIP: fix attempt n

--- Backend/server/Server.ts
+++ Backend/server/Server.ts
@@ -24,26 +24,26 @@ export class Server {
 
   private configureMiddleware(): void {
     // CORS middleware
-    const allowCrossDomain = (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) => {
-      res.header(`Access-Control-Allow-Origin`, [
-        Globals.FRONTEND_URL,
-        Globals.API_URL
-      ]);
-      res.header(`Access-Control-Allow-Methods`, [
-        'GET',
-        'PUT',
-        'POST',
-        'DELETE'
-      ]);
-      res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-      next();
-    };
+    // const allowCrossDomain = (
+    //   req: Request,
+    //   res: Response,
+    //   next: NextFunction
+    // ) => {
+    //   res.header(`Access-Control-Allow-Origin`, [
+    //     Globals.FRONTEND_URL,
+    //     Globals.API_URL
+    //   ]);
+    //   res.header(`Access-Control-Allow-Methods`, [
+    //     'GET',
+    //     'PUT',
+    //     'POST',
+    //     'DELETE'
+    //   ]);
+    //   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    //   next();
+    // };
 
-    this.app.use(allowCrossDomain);
+    // this.app.use(allowCrossDomain);
 
     this.app.use(cookieParser());
     this.app.use(bodyParser.urlencoded());
