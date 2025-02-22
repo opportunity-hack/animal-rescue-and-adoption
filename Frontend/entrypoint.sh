afbb2a74d5b73e10b1b95425d22a5b21be534e34
Greg V <greg.vannoni@gmail.com>
2025-02-22 15:53:58 -0700

    [Fly deployment] No need to use nginx, just use vite serve

--- Frontend/entrypoint.sh
+++ Frontend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Replace $VITE_G_API_URL with the environment variable value
-envsubst '$VITE_G_API_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+# Generate final nginx.conf using environment variables
+envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Nginx
-nginx -g 'daemon off;'
+exec "$@"
